c21ea89f7eb87e3c0fd892cda0cbe823737b5c11
agno <crash.pc89@gmail.com>
2020-02-26 00:24:45 +0100

    Update main app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { AngularFireDatabase  } from '@angular/fire/database';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,8 +7,6 @@ import { Observable } from 'rxjs';
 })
 export class AppComponent {
   title = 'ng-gippy';
-  items : Observable<any[]>;
-  constructor(db: AngularFireDatabase) {
-  	this.items = db.list('gippy/bills').valueChanges();
-  }
+
+  constructor() {}
 }
