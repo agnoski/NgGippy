d2637724a0b9310694eaa29837d2cfc39b92ef0f
agno <crash.pc89@gmail.com>
2020-02-26 00:22:49 +0100

    Add new routes to form, list and summary components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { BillFormComponent } from './bills/bill-form/bill-form.component';
 import { BillsListComponent } from './bills/bills-list/bills-list.component';
+import { BillsSummaryComponent } from './bills/bills-summary/bills-summary.component';
 
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-{ path: '', redirectTo: 'bills', pathMatch: 'full' },
-{ path: 'bills', component: BillsListComponent },
-{ path: 'form', component: BillFormComponent }
+	{ path: 'bills', component: BillsListComponent },
+	{ path: 'form', component: BillFormComponent },
+	{ path: 'summary', component: BillsSummaryComponent }
+
 ];
 
 @NgModule({
