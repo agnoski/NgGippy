bd7ab1a04cc48e478e49f27f272e450c25297224
agno <crash.pc89@gmail.com>
2020-02-25 23:43:35 +0100

    Change app-routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
+import { BillFormComponent } from './bills/bill-form/bill-form.component';
+import { BillsListComponent } from './bills/bills-list/bills-list.component';
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+{ path: '', redirectTo: 'bills', pathMatch: 'full' },
+{ path: 'bills', component: BillsListComponent },
+{ path: 'form', component: BillFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
