0cf8433cc93918191f50a1d64d4b0d22bbb2a0eb
agno <crash.pc89@gmail.com>
2020-02-26 00:23:58 +0100

    Update modules with new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,19 +9,22 @@ import { AngularFireModule } from '@angular/fire';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
 
 import { environment } from '../environments/environment';
-import { DataformComponent } from './dataform/dataform.component';
-import { SummaryComponent } from './summary/summary.component';
-import { IndexComponent } from './index/index.component';
+import { BillDetailsComponent } from './bills/bill-details/bill-details.component';
+import { BillsListComponent } from './bills/bills-list/bills-list.component';
+import { BillFormComponent } from './bills/bill-form/bill-form.component';
+import { BillsSummaryComponent } from './bills/bills-summary/bills-summary.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DataformComponent,
-    SummaryComponent,
-    IndexComponent
+    BillDetailsComponent,
+    BillsListComponent,
+    BillFormComponent,
+    BillsSummaryComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AngularFireModule.initializeApp(environment.firebase), // imports firebase/app needed for everything
     AngularFireDatabaseModule, // imports firebase/firestore, only needed for database features
     AppRoutingModule
