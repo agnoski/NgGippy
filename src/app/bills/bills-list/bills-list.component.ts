b6faa3957f6730bead64a1d4e655e55fd12db802
agno <crash.pc89@gmail.com>
2020-02-25 23:05:58 +0100

    Add empty bills-list component

---
+++ src/app/bills/bills-list/bills-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bills-list',
+  templateUrl: './bills-list.component.html',
+  styleUrls: ['./bills-list.component.css']
+})
+export class BillsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
