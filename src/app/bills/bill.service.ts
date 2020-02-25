2ab4d1a523f36cc322893ca98ac339be7cc9a401
agno <crash.pc89@gmail.com>
2020-02-25 22:43:45 +0100

    Add bill.service logic

--- src/app/bills/bill.service.ts
+++ src/app/bills/bill.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+import { Bill } from './bill';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BillService {
+	private dbPath = '/gippy/bills';
 
-  constructor() { }
+	billsRef : AngularFireList<Bill> = null;
+	
+	constructor(private db: AngularFireDatabase) {
+ 		this.billsRef = db.list(dbPath);
+	}
+
+	addBill(bill: Bill): void {
+		this.billsRef.push(bill);
+	}
+
+	updateBill(key: string, value: any): Promise<void> {
+		return this.billsRef.update(key, value);
+	}
+
+	deleteBill(key: string): Promise<void> {
+		return this.billsRef.remove(key);
+	}
+
+	getBillsList(): AngularFireList<Bill> {
+		return this.billsRef;
+	}
 }
