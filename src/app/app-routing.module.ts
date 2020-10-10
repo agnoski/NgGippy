27c4edd5caaf95efce228b6da5340d1558c69772
agno <crash.pc89@gmail.com>
2020-10-10 18:32:53 +0200

    Add bills-home routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,5 @@
 import { LoginComponent } from './login/login.component';
+import { BillsHomeComponent } from './bills/bills-home/bills-home.component';
 import { BillFormComponent } from './bills/bill-form/bill-form.component';
 import { BillsListComponent } from './bills/bills-list/bills-list.component';
 import { BillsSummaryComponent } from './bills/bills-summary/bills-summary.component';
@@ -9,6 +10,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
 	{ path: 'login', component: LoginComponent },
+	{ path: 'home', component: BillsHomeComponent },
 	{ path: 'bills', component: BillsListComponent },
 	{ path: 'form', component: BillFormComponent },
 	{ path: 'summary', component: BillsSummaryComponent }
