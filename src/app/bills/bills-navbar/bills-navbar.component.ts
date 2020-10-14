c9f28d8fca624aee26fe49086d7b22da67ab83da
agno <crash.pc89@gmail.com>
2020-10-14 22:57:48 +0200

    Fix collapsed navbar menu for mobile

--- src/app/bills/bills-navbar/bills-navbar.component.ts
+++ src/app/bills/bills-navbar/bills-navbar.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BillsNavbarComponent implements OnInit {
 
+  title = "Gippy";
+  isCollapsed = true;
+
   constructor() { }
 
   ngOnInit(): void {
