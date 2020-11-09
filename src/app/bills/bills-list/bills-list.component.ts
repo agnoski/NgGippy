d4ac0998724b64a8e281d7c7438ba41449c534a2
agno <crash.pc89@gmail.com>
2020-11-09 01:11:23 +0100

    Add bills search engine

--- src/app/bills/bills-list/bills-list.component.ts
+++ src/app/bills/bills-list/bills-list.component.ts
@@ -10,8 +10,17 @@ import { map } from 'rxjs/operators';
 export class BillsListComponent implements OnInit {
 
 	bills: any;
+	billsFiltered: any;
+  funcList: any;
 
-	constructor(private billService: BillService) { }
+	constructor(private billService: BillService) {
+    this.funcList = {
+      "@": (bill, user) => bill.user === user,
+      "#": (bill, category) => bill.category === category,
+      ":": (bill, shop) => bill.shop === shop,
+      "/": (bill, date) => bill.date === date
+    }
+  }
 
 	ngOnInit(): void {
 		this.getBillsList();
@@ -26,7 +35,36 @@ export class BillsListComponent implements OnInit {
 			)
 		).subscribe(bills => {
 			this.bills = bills;
+			this.billsFiltered = bills;
 		});
 	}
+
+  onFilter(event) {
+    const str = event.target.value;
+    console.log(str);
+
+    // parse space
+    const tokens = str.split(" ");
+
+    // take first caracter
+    // map to a list of filter functions
+    const filtFun = tokens.filter(token => token.length > 1).map(token => {
+      return {func: this.funcList[token[0]], arg: token.slice(1)};
+    });
+
+    // apply filters
+    let billsLocal = this.bills.filter(bill => true); // copy
+    filtFun.forEach(filt => {
+      if(filt.func) {
+        billsLocal = billsLocal.filter(bill => filt.func(bill, filt.arg)); 
+      }
+    });
+
+    this.billsFiltered = billsLocal;
+  }
+
+  filterByCategory(category) {
+    this.billsFiltered = this.bills.filter(bill => category === "" ? true : bill.category === category);
+  }
 
 }
