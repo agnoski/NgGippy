249b9698e9d635b354f1a3d319e316551e7e72bd
agno <crash.pc89@gmail.com>
2020-11-09 16:07:24 +0100

    Use BillsFilter class in BillsFilter webworker

--- src/app/web-workers/bills-filter.worker.ts
+++ src/app/web-workers/bills-filter.worker.ts
@@ -1,32 +1,9 @@
 /// <reference lib="webworker" />
+import { BillsFilter } from '../classes/bills-filter'
 
-const funcList = {
-  "@": (bill, user) => bill.user === user,
-  "#": (bill, category) => bill.category === category,
-  ":": (bill, shop) => bill.shop === shop,
-  "/": (bill, date) => bill.date === date
-}
+const billsFilter = new BillsFilter();
 
 addEventListener('message', ({ data }) => {
-  // parse space
-  const tokens = data.str.split(" ");
-
-  // take first caracter
-  // map to a list of filter functions
-  const filtFun = tokens.filter(token => token.length > 1).map(token => {
-    return {
-      func: funcList[token[0]],
-      arg: token.slice(1)
-    };
-  });
-
-  // apply filters
-  let billsLocal = data.bills.filter(bill => true); // copy
-  filtFun.forEach(filt => {
-    if(filt.func) {
-      billsLocal = billsLocal.filter(bill => filt.func(bill, filt.arg));
-    }
-  });
-
+  const billsLocal = billsFilter.filter(data.str, data.bills);
   postMessage(billsLocal);
 });
